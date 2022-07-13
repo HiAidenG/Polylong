39989c316ff37b2cb5fbaea0652a3dd7b4bbdbf6
HiAidenG <101067335+HiAidenG@users.noreply.github.com>
2022-07-13 15:32:32 -0400

    Update get_seqs.sh

--- get_seqs.sh
+++ get_seqs.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 set -euxo pipefail
 #This script collects all protein coding sequences and unstable sequences -200bp and +312bp from TSS.
-#Run this in a separate directory... it creates a lot of files.
 
 #Download reference genome
-# mkdir data
-# curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/gencode.v40.annotation.gtf.gz | gunzip -d > data/gencode.v40.annotation.gtf
+mkdir data
+curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/gencode.v40.annotation.gtf.gz | gunzip -d > data/gencode.v40.annotation.gtf
 
 #take all entries that are protein coding and do not have a transcript ID
 #these should be the coordinates for all protein coding genes
-# grep 'gene_type "protein_coding"' data/gencode.v40.annotation.gtf | grep -v 'transcript_id' > data/gencode.v40.annotation.pc.gtf
+grep 'gene_type "protein_coding"' data/gencode.v40.annotation.gtf | grep -v 'transcript_id' > data/gencode.v40.annotation.pc.gtf
 
 #remove any potential duplicate gene names
-# cat data/gencode.v40.annotation.pc.gtf | awk 'BEGIN { FS = ";" } ; !seen[$3]++' > data/gencode.v40.annotation.pc.unique.gtf
+cat data/gencode.v40.annotation.pc.gtf | awk 'BEGIN { FS = ";" } ; !seen[$3]++' > data/gencode.v40.annotation.pc.unique.gtf
 
-# wc -l data/gencode.v40.annotation.pc.unique.gtf
+wc -l data/gencode.v40.annotation.pc.unique.gtf
 
 #get sequences from primary assembly fasta for protein coding genes; flat format
 #WARNING: This is inefficent but it works and I don't know a better way ATM.
-# curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/GRCh38.primary_assembly.genome.fa.gz | gunzip -d > data/GRCh38.primary_assembly.genome.fa
+curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/GRCh38.primary_assembly.genome.fa.gz | gunzip -d > data/GRCh38.primary_assembly.genome.fa
 seqkit subseq --gtf data/gencode.v40.annotation.pc.unique.gtf data/GRCh38.primary_assembly.genome.fa -u 200 -d 312 > data/gencode.v40.annotation.pc.unique.fa
 
 #should match above wc call for the .gtf file
