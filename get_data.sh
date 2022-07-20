58df48824effbaa76106b87a382af26d24e25f22
HiAidenG <101067335+HiAidenG@users.noreply.github.com>
2022-07-20 14:28:12 -0400

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 #This script collects all protein coding sequences and unstable sequences -200bp and +312bp from TSS.
 
 #Download reference genome
-# mkdir data
-# curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/gencode.v40.annotation.gtf.gz | gunzip -d > data/gencode.v40.annotation.gtf
+mkdir data
+curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/gencode.v40.annotation.gtf.gz | gunzip -d > data/gencode.v40.annotation.gtf
 
 #take all entries that are protein coding and do not have a transcript ID
 #these should be the coordinates for all protein coding genes
@@ -17,7 +17,7 @@ wc -l data/gencode.v40.annotation.pc.unique.gtf
 
 #get sequences from primary assembly fasta for protein coding genes; flat format
 #WARNING: This is inefficent but it works and I don't know a better way ATM.
-# curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/GRCh38.primary_assembly.genome.fa.gz | gunzip -d > data/GRCh38.primary_assembly.genome.fa
+curl https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/GRCh38.primary_assembly.genome.fa.gz | gunzip -d > data/GRCh38.primary_assembly.genome.fa
 seqkit subseq --gtf data/gencode.v40.annotation.pc.unique.gtf data/GRCh38.primary_assembly.genome.fa -u 1024 -d 1024 > data/gencode.v40.annotation.pc.unique.fa
 
 #should match above wc call for the .gtf file
